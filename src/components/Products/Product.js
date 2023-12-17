efdc1e0663f74d850822cc15784d964f46f477be
Fuyad <fuyad56@gmail.com>
2023-12-17 20:22:49 +0600

    nothing

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -529,7 +529,7 @@ export default function Product() {
               <a
                 key={product.id}
                 href={product.href}
-                className="group"
+                className="group border p-4 rounded-xl shadow-md hover:scale-[1.07] duration-300"
                 onClick={() => handleProduct(product.href)}
               >
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
@@ -539,7 +539,7 @@ export default function Product() {
                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                   />
                 </div>
-                <h3 className="mt-4 text-xl text-center text-gray-700">{product.name}</h3>
+                <h3 className="mt-4 text-xl font-medium text-center text-gray-700">{product.name}</h3>
               </a>
             ))}
           </div>
